0f1bb344ff3ed7beec75db8e15bd9d4a50c2598a
Thomas LEVEIL <thomasleveil@gmail.com>
2017-01-14 12:30:48 +0100

    TESTS: port tests for #590 to docker-compose test

---
+++ test2/test_ssl_nohttps.py
@@ -0,0 +1,13 @@
+import pytest
+from requests import ConnectionError
+
+def test_http_is_forwarded(docker_compose, nginxproxy):
+    r = nginxproxy.get("http://web.nginx-proxy.tld/port", allow_redirects=False)
+    assert r.status_code == 200
+    assert "answer from port 83\n" in r.text
+
+
+def test_https_is_disabled(docker_compose, nginxproxy):
+    with pytest.raises(ConnectionError) as excinfo:
+        r = nginxproxy.get("https://web.nginx-proxy.tld/", allow_redirects=False)
+        assert "[Errno 93] Protocol not supportedd" in excinfo.value
