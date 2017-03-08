abdd5883a1cc5a6db496f6cb8b7016491b0b7e12
Thomas LEVEIL <thomasleveil@gmail.com>
2017-03-08 02:37:12 +0100

    TESTS: refactor dhparam tests

--- test/test_ssl/test_dhparam_generation.py
+++ test/test_ssl/test_dhparam_generation.py
@@ -1,52 +1,42 @@
-import pytest
-import os
+import backoff
 import docker
-import time
 
 docker_client = docker.from_env()
 
-def wait_for_nginxproxy_to_be_ready():
+
+###############################################################################
+#
+# Tests helpers
+#
+###############################################################################
+
+@backoff.on_exception(backoff.constant, AssertionError, interval=2, max_tries=15, jitter=None)
+def assert_log_contains(expected_log_line):
     """
-    If one (and only one) container started from image jwilder/nginx-proxy:test is found,
-    wait for its log to contain substring "Watching docker events"
+    Check that the nginx-proxy container log contains a given string.
+    The backoff decorator will retry the check 15 times with a 2 seconds delay.
+
+    :param expected_log_line: string to search for
+    :return: None
+    :raises: AssertError if the expected string is not found in the log
     """
-    containers = docker_client.containers.list(filters={"ancestor": "jwilder/nginx-proxy:test"})
-    if len(containers) != 1:
-        return
-    container = containers[0]
-    for line in container.logs(stream=True):
-        if "Watching docker events" in line:
-            break
-
-def test_dhparam_is_generated_if_missing(docker_compose, nginxproxy):
-    wait_for_nginxproxy_to_be_ready()
-
-    containers = docker_client.containers.list(filters={"ancestor": "jwilder/nginx-proxy:test"})
-    if len(containers) != 1:
-        assert 0
-        return
-
-    sut_container = containers[0]
-
+    sut_container = docker_client.containers.get("nginxproxy")
     docker_logs = sut_container.logs(stdout=True, stderr=True, stream=False, follow=False)
+    assert expected_log_line in docker_logs
 
-    assert "Generating DH parameters" in docker_logs
 
-    expected_line = "dhparam generation complete, reloading nginx"
-    max_wait = 30
-    sleep_interval = 2
-    current_wait = 0
+###############################################################################
+#
+# Tests
+#
+###############################################################################
 
-    while current_wait < max_wait:
-        docker_logs = sut_container.logs(stdout=True, stderr=True, stream=False, follow=False)
-        if expected_line in docker_logs:
-            break
+def test_dhparam_is_generated_if_missing(docker_compose):
+    sut_container = docker_client.containers.get("nginxproxy")
+    assert sut_container.status == "running"
 
-        time.sleep(sleep_interval)
-        current_wait += sleep_interval
-
-    # Re-check the logs to get better assert output on failure
-    assert expected_line in docker_logs
+    assert_log_contains("Generating DH parameters")
+    assert_log_contains("dhparam generation complete, reloading nginx")
 
     # Make sure the dhparam in use is not the default, pre-generated one
     default_checksum = sut_container.exec_run("md5sum /app/dhparam.pem.default").split()
