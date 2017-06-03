ecb0e29a2c3e372f60261b12fe0551d8de5b9dcb
neilpang <github@byneil.com>
2017-06-03 17:21:23 +0800

    fix acme

--- updatessl.sh
+++ updatessl.sh
@@ -6,7 +6,6 @@ ACME_BIN="/acme.sh/acme.sh --home /acme.sh --config-home /acmecerts"
 
 DEFAULT_CONF="/etc/nginx/conf.d/default.conf"
 
-NGINX_HOME="/etc/nginx"
 
 CERTS="/etc/nginx/certs"
 
@@ -19,9 +18,7 @@ updatessl() {
       d=$(echo "$d_list" | cut -d , -f 1)
       $ACME_BIN --issue \
       -d $d_list \
-      -w $NGINX_HOME/html \
-      --pre-hook "$_SCRIPT_ pre_hook $DEFAULT_CONF" \
-      --post-hook "$_SCRIPT_ post_hook $DEFAULT_CONF" \
+      --nginx \
       --fullchain-file "$CERTS\$d.crt" \
       --key-file "$CERTS\$d.crt" \
       --reloadcmd "service nginx configtest && service force-reload"
@@ -35,17 +32,6 @@ updatessl() {
   fi
 }
 
-
-
-pre_hook() {
-  _d_conf="$1"
-  sed -i "s|#\(location.*#acme\)|\\1|" $_d_conf && service nginx configtest && service force-reload
-}
-
-post_hook() {
-  _d_conf="$1"
-  sed -i "s|\(location.*#acme\)|#\\1|"  $_d_conf 
-}
 
 
 "$@"
