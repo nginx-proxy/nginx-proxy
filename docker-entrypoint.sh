5d503b48cb414026c268069b273aef2ce6708b0f
Steve Kamerman <stevekamerman@gmail.com>
2017-10-19 20:58:34 -0400

    Added support for IPv6 DNS servers in resolver generation (issue #938)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,8 +18,8 @@ fi
 # Note: if $DHPARAM_BITS is not defined, generate-dhparam.sh will use 2048 as a default
 /app/generate-dhparam.sh $DHPARAM_BITS
 
-# Compute the DNS resolvers for use in the templates
-export RESOLVERS=$(awk '$1 == "nameserver" {print $2}' ORS=' ' /etc/resolv.conf | sed 's/ *$//g')
+# Compute the DNS resolvers for use in the templates - if the IP contains ":", it's IPv6 and must be enclosed in []
+export RESOLVERS=$(awk '$1 == "nameserver" {print ($2 ~ ":")? "["$2"]": $2}' ORS=' ' /etc/resolv.conf | sed 's/ *$//g')
 if [ "x$RESOLVERS" = "x" ]; then
     echo "Warning: unable to determine DNS resolvers for nginx" >&2
     unset RESOLVERS
