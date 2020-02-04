8d47538172751b6ae2244d660fc47c42f3e5d33b
Constantin Wildförster <constantin@wildfoerster.org>
2020-02-04 20:43:04 +0100

    add virtual path tests

--- test/test_virtual_path.py
+++ test/test_virtual_path.py
@@ -6,18 +6,16 @@ def test_default(docker_compose, nginxproxy):
     assert r.status_code == 200
     assert r.text == "answer from port 81\n"
 
-def test_simple_path(docker_compose, nginxproxy):
+def test_simple_path_not_stripped(docker_compose, nginxproxy):
     r = nginxproxy.get("http://nginx-proxy.test/foo/port")
-    assert r.status_code == 200
-    assert r.text == "answer from port 82\n"
+    assert r.status_code == 404
 
-def test_deep_path(docker_compose, nginxproxy):
-    r = nginxproxy.get("http://nginx-proxy.test/bar/even/deeper/port")
+def test_simple_path_stripped(docker_compose, nginxproxy):
+    r = nginxproxy.get("http://nginx-proxy.test/bar/port")
     assert r.status_code == 200
     assert r.text == "answer from port 83\n"
 
-def test_closed_path(docker_compose, nginxproxy):
-    r = nginxproxy.get("http://nginx-proxy.test/bar/even/deeper/with/end/port")
+def test_deep_path_not_stripped(docker_compose, nginxproxy):
+    r = nginxproxy.get("http://nginx-proxy.test/deep/path/port")
     assert r.status_code == 200
     assert r.text == "answer from port 84\n"
-
