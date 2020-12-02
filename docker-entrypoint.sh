aba100ff7b27251aceb20d80d7e6abeff0b42665
Thibault Soubiran <thibault.soubiran@protonmail.com>
2020-12-02 13:18:32 +0100

    Update docker-entrypoint.sh
    Default key size is not 2048 anymore but 4096, reference : https://github.com/nginx-proxy/nginx-proxy/blob/master/generate-dhparam.sh#L4

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,7 +15,7 @@ if [[ $DOCKER_HOST = unix://* ]]; then
 fi
 
 # Generate dhparam file if required
-# Note: if $DHPARAM_BITS is not defined, generate-dhparam.sh will use 2048 as a default
+# Note: if $DHPARAM_BITS is not defined, generate-dhparam.sh will use 4096 as a default
 # Note2: if $DHPARAM_GENERATION is set to false in environment variable, dh param generator will skip completely
 /app/generate-dhparam.sh $DHPARAM_BITS $DHPARAM_GENERATION
 
