9bad218f2344b3d8a8591a84b5547891e646b35c
Dakota Hawkins <dakotahawkins@gmail.com>
2021-05-05 17:09:25 -0400

    Add vhost.d includes to nginx.tmpl's https redirect server block
    Adds customization points to https redirect responses that are analogous
    to other response handling. This gives you the opportunity to add
    response headers or etc. before returning the 301 redirect.
    
    If `$host` or `default` exist in `/etc/nginx/vhost.d/`, we rely on
    nginx-proxy/acme-companion to add the Let's Encrypt acme challenge
    location block there, so it's only included here if those files don't
    exist.
    
    New tests in `test_ssl` are similar to tests in `test_custom`, except they
    expect 301 responses along with custom configs.
    
    Fixes #1613

---
+++ test/test_ssl/test_redirect_custom_defaults-location.py
@@ -0,0 +1,27 @@
+import pytest
+
+def test_custom_default_conf_does_not_apply_to_unknown_vhost(docker_compose, nginxproxy):
+    r = nginxproxy.get("http://nginx-proxy/", allow_redirects=False)
+    assert r.status_code == 503
+    assert "X-test" not in r.headers
+
+def test_custom_default_conf_applies_to_web2(docker_compose, nginxproxy):
+    url = "web2.nginx-proxy.tld/port"
+    r = nginxproxy.get(f"http://{url}", allow_redirects=False)
+    assert r.status_code == 301
+    assert "301 Moved Permanently" in r.text
+    assert "X-test" in r.headers
+    assert "f00" == r.headers["X-test"]
+    assert "Location" in r.headers
+    assert f"https://{url}" == r.headers["Location"]
+
+
+def test_custom_default_conf_is_overriden_for_web3(docker_compose, nginxproxy):
+    url = "web3.nginx-proxy.tld/port"
+    r = nginxproxy.get(f"http://{url}", allow_redirects=False)
+    assert r.status_code == 301
+    assert "301 Moved Permanently" in r.text
+    assert "X-test" in r.headers
+    assert "bar" == r.headers["X-test"]
+    assert "Location" in r.headers
+    assert f"https://{url}" == r.headers["Location"]
