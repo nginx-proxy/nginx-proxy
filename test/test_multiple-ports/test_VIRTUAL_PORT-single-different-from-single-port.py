eaef4429f262263f70e05ce356737c3da53003f6
Nicolas Duchon <nicolas.duchon@gmail.com>
2021-06-09 09:25:51 +0200

    test: update failing test due to max_fails=0

--- test/test_multiple-ports/test_VIRTUAL_PORT-single-different-from-single-port.py
+++ test/test_multiple-ports/test_VIRTUAL_PORT-single-different-from-single-port.py
@@ -5,4 +5,4 @@ import re
 def test_answer_is_served_from_virtual_port_which_is_ureachable(docker_compose, nginxproxy):
     r = nginxproxy.get("http://web.nginx-proxy.tld/port")
     assert r.status_code == 502
-    assert re.search(r"\n\s+server \d+\.\d+\.\d+\.\d+:90;\n", nginxproxy.get_conf().decode('ASCII'))
+    assert re.search(r"\n\s+server \d+\.\d+\.\d+\.\d+:90 max_fails=0;\n", nginxproxy.get_conf().decode('ASCII'))
