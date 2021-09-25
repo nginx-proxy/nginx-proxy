6f7eb8bd97de5db8fbe1f22d8291288bc595697e
polarathene <5098581+polarathene@users.noreply.github.com>
2021-09-25 16:06:11 +1200

    chore: 6/6 - Update shell syntax
    - `==` for string equality since we're using bash `[[ test ]]` already.
    - Uppercase `socket_file` variable to be consistent with other internal variables used in the script.
    - Convert `[ test ]` to `[[ test ]]` for consistency, improving maintenance. Double-bracket (_not posix compatible_) does not require quoted variables, ShellCheck lint knows this is safe too :)
    - `-z` test for `$RESOLVERS` is native syntax to check for empty string value.
    - Referenced variables should generally be wrapped like so `"${VAR}"`.
    - Variable assignments with string values should be double quotes for content with variables, otherwise use single quotes (_no interpolation_).
    - Converted my if statements to use the same style used in the rest of the file.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,12 +3,13 @@ set -e
 
 function _check_unix_socket() {
 	# Warn if the DOCKER_HOST socket does not exist
-	if [[ $DOCKER_HOST = unix://* ]]; then
-		socket_file=${DOCKER_HOST#unix://}
-		if ! [ -S "$socket_file" ]; then
+	if [[ ${DOCKER_HOST} == unix://* ]]; then
+		local SOCKET_FILE="${DOCKER_HOST#unix://}"
+
+		if [[ ! -S ${SOCKET_FILE} ]]; then
 			cat >&2 <<-EOT
-				ERROR: you need to share your Docker host socket with a volume at $socket_file
-				Typically you should run your nginxproxy/nginx-proxy with: \`-v /var/run/docker.sock:$socket_file:ro\`
+				ERROR: you need to share your Docker host socket with a volume at ${SOCKET_FILE}
+				Typically you should run your nginxproxy/nginx-proxy with: \`-v /var/run/docker.sock:${SOCKET_FILE}:ro\`
 				See the documentation at http://git.io/vZaGJ
 			EOT
 
@@ -21,15 +22,15 @@ function _resolvers() {
 	# Compute the DNS resolvers for use in the templates - if the IP contains ":", it's IPv6 and must be enclosed in []
 	RESOLVERS=$(awk '$1 == "nameserver" {print ($2 ~ ":")? "["$2"]": $2}' ORS=' ' /etc/resolv.conf | sed 's/ *$//g'); export RESOLVERS
 
-	SCOPED_IPV6_REGEX="\[fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}\]"
+	SCOPED_IPV6_REGEX='\[fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}\]'
 
-	if [ "$RESOLVERS" = "" ]; then
-		echo "Warning: unable to determine DNS resolvers for nginx" >&2
+	if [[ -z ${RESOLVERS} ]]; then
+		echo 'Warning: unable to determine DNS resolvers for nginx' >&2
 		unset RESOLVERS
-	elif [[ $RESOLVERS =~ $SCOPED_IPV6_REGEX ]]; then
-		echo -n "Warning: Scoped IPv6 addresses removed from resolvers: " >&2
-		echo "$RESOLVERS" | grep -Eo "$SCOPED_IPV6_REGEX" | paste -s -d ' ' >&2
-		RESOLVERS=$(echo "$RESOLVERS" | sed -r "s/$SCOPED_IPV6_REGEX//g" | xargs echo -n); export RESOLVERS
+	elif [[ ${RESOLVERS} =~ ${SCOPED_IPV6_REGEX} ]]; then
+		echo -n 'Warning: Scoped IPv6 addresses removed from resolvers: ' >&2
+		echo "${RESOLVERS}" | grep -Eo "$SCOPED_IPV6_REGEX" | paste -s -d ' ' >&2
+		RESOLVERS=$(echo "${RESOLVERS}" | sed -r "s/${SCOPED_IPV6_REGEX}//g" | xargs echo -n); export RESOLVERS
 	fi
 }
 
@@ -41,8 +42,7 @@ function _setup_dhparam() {
 
 	# DH params may be provided by the user (rarely necessary),
 	# or use an existing pre-generated group from RFC7919, defaulting to 4096-bit:
-	if [[ -f ${DHPARAM_FILE} ]]
-	then
+	if [[ -f ${DHPARAM_FILE} ]]; then
 		echo 'Warning: A custom dhparam.pem file was provided. Best practice is to use standardized RFC7919 DHE groups instead.' >&2
 	else
 		# ENV DHPARAM_BITS - Defines which RFC7919 DHE group to use (default: 4096-bit):
@@ -53,9 +53,8 @@ function _setup_dhparam() {
 
 		# Only the following pre-generated sizes are supported,
 		# emit an error and kill the container if provided an invalid value:
-		if [[ ! ${DHPARAM_BITS} =~ ^(2048|3072|4096)$ ]]
-		then
-			echo "ERROR: Unsupported DHPARAM_BITS size: ${DHPARAM_BITS}, use 2048, 3072, or 4096 (default)." >&2
+		if [[ ! ${DHPARAM_BITS} =~ ^(2048|3072|4096)$ ]]; then
+			echo "ERROR: Unsupported DHPARAM_BITS size: ${DHPARAM_BITS}. Use: 2048, 3072, or 4096 (default)." >&2
 			exit 1
 		fi
 
