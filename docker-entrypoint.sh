091dd855afca83b727253d32e347bcf0948a2d56
polarathene <5098581+polarathene@users.noreply.github.com>
2021-09-25 15:47:20 +1200

    chore: 3/6 - Indent function content
    This is a white-space only change to ease review diff noise.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -33,36 +33,35 @@ function _setup_dhparam() {
 }
 
 function _init() {
-# Warn if the DOCKER_HOST socket does not exist
-if [[ $DOCKER_HOST = unix://* ]]; then
-	socket_file=${DOCKER_HOST#unix://}
-	if ! [ -S "$socket_file" ]; then
-		cat >&2 <<-EOT
-			ERROR: you need to share your Docker host socket with a volume at $socket_file
-			Typically you should run your nginxproxy/nginx-proxy with: \`-v /var/run/docker.sock:$socket_file:ro\`
-			See the documentation at http://git.io/vZaGJ
-		EOT
+	# Warn if the DOCKER_HOST socket does not exist
+	if [[ $DOCKER_HOST = unix://* ]]; then
+		socket_file=${DOCKER_HOST#unix://}
+		if ! [ -S "$socket_file" ]; then
+			cat >&2 <<-EOT
+				ERROR: you need to share your Docker host socket with a volume at $socket_file
+				Typically you should run your nginxproxy/nginx-proxy with: \`-v /var/run/docker.sock:$socket_file:ro\`
+				See the documentation at http://git.io/vZaGJ
+			EOT
 
-		exit 1
-
+			exit 1
+		fi
 	fi
-fi
 
-_setup_dhparam
+	_setup_dhparam
 
-# Compute the DNS resolvers for use in the templates - if the IP contains ":", it's IPv6 and must be enclosed in []
-RESOLVERS=$(awk '$1 == "nameserver" {print ($2 ~ ":")? "["$2"]": $2}' ORS=' ' /etc/resolv.conf | sed 's/ *$//g'); export RESOLVERS
+	# Compute the DNS resolvers for use in the templates - if the IP contains ":", it's IPv6 and must be enclosed in []
+	RESOLVERS=$(awk '$1 == "nameserver" {print ($2 ~ ":")? "["$2"]": $2}' ORS=' ' /etc/resolv.conf | sed 's/ *$//g'); export RESOLVERS
 
-SCOPED_IPV6_REGEX="\[fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}\]"
+	SCOPED_IPV6_REGEX="\[fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}\]"
 
-if [ "$RESOLVERS" = "" ]; then
-	echo "Warning: unable to determine DNS resolvers for nginx" >&2
-	unset RESOLVERS
-elif [[ $RESOLVERS =~ $SCOPED_IPV6_REGEX ]]; then
-	echo -n "Warning: Scoped IPv6 addresses removed from resolvers: " >&2
-	echo "$RESOLVERS" | grep -Eo "$SCOPED_IPV6_REGEX" | paste -s -d ' ' >&2
-	RESOLVERS=$(echo "$RESOLVERS" | sed -r "s/$SCOPED_IPV6_REGEX//g" | xargs echo -n); export RESOLVERS
-fi
+	if [ "$RESOLVERS" = "" ]; then
+		echo "Warning: unable to determine DNS resolvers for nginx" >&2
+		unset RESOLVERS
+	elif [[ $RESOLVERS =~ $SCOPED_IPV6_REGEX ]]; then
+		echo -n "Warning: Scoped IPv6 addresses removed from resolvers: " >&2
+		echo "$RESOLVERS" | grep -Eo "$SCOPED_IPV6_REGEX" | paste -s -d ' ' >&2
+		RESOLVERS=$(echo "$RESOLVERS" | sed -r "s/$SCOPED_IPV6_REGEX//g" | xargs echo -n); export RESOLVERS
+	fi
 }
 
 # Run the init logic if the default CMD was provided
