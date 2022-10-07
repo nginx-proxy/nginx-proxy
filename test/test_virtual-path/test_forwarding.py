727264995857bd959fa785d31255903cb61b997c
Robin Windey <ro.windey@gmail.com>
2022-10-07 15:40:44 +0200

    Fix tests after merge with upstream
    * Use "sub." instead of "www." because the latter will be redirected to non-www

--- test/test_virtual-path/test_forwarding.py
+++ test/test_virtual-path/test_forwarding.py
@@ -1,18 +1,18 @@
 import pytest
 
 def test_root_redirects_to_web1(docker_compose, nginxproxy):
-    r = nginxproxy.get("http://www.nginx-proxy.tld/port", allow_redirects=False)
+    r = nginxproxy.get("http://sub.nginx-proxy.tld/port", allow_redirects=False)
     assert r.status_code == 301
     assert "Location" in r.headers
-    assert "http://www.nginx-proxy.tld/web1/port" == r.headers['Location']
+    assert "http://sub.nginx-proxy.tld/web1/port" == r.headers['Location']
 
 def test_direct_access(docker_compose, nginxproxy):
-    r = nginxproxy.get("http://www.nginx-proxy.tld/web1/port", allow_redirects=False)
+    r = nginxproxy.get("http://sub.nginx-proxy.tld/web1/port", allow_redirects=False)
     assert r.status_code == 200
     assert "answer from port 81\n" in r.text
 
 def test_root_is_forwarded(docker_compose, nginxproxy):
-    r = nginxproxy.get("http://www.nginx-proxy.tld/port", allow_redirects=True)
+    r = nginxproxy.get("http://sub.nginx-proxy.tld/port", allow_redirects=True)
     assert r.status_code == 200
     assert "answer from port 81\n" in r.text
 
