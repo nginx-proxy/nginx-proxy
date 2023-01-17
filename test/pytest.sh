d56b5b370d8aef801ec2c1d7c13dfcf2a6f23c03
Richard Hansen <rhansen@rhansen.org>
2023-01-16 23:37:48 -0500

    tests: Whitespace fixes

--- test/pytest.sh
+++ test/pytest.sh
@@ -16,7 +16,7 @@ ARGS=("$@")
 echo "> Building nginx-proxy-tester image..."
 docker build -t nginx-proxy-tester -f "${DIR}/requirements/Dockerfile-nginx-proxy-tester" "${DIR}/requirements"
 
-# run the nginx-proxy-tester container setting the correct value for the working dir in order for 
+# run the nginx-proxy-tester container setting the correct value for the working dir in order for
 # docker-compose to work properly when run from within that container.
 exec docker run --rm -it --name "nginx-proxy-pytest" \
   --volume "/var/run/docker.sock:/var/run/docker.sock" \
