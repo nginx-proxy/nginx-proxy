8fbc8514ef3cd4a464c0ed7315590f8dee8b972c
Richard Hansen <rhansen@rhansen.org>
2023-01-17 21:23:08 -0500

    feat: Unconditionally produce debug comments
    Rationale for eliminating the check to see if the `DEBUG` environment
    variable holds a true value:
      * The `DEBUG` environment variable might be set on a container (for
        purposes specific to that container, not `nginx-proxy`) to a value
        that cannot be parsed as a bool, which would break `nginx-proxy`.
      * It simplifies the template.
      * It eliminates a cold code path.
      * It avoids heisenbugs.
      * It makes debugging easier for users.
    
    Also delete the debug info tests, as they are fragile and they provide
    limited value.
    
    Alternatively, we could avoid collision with the container's use of
    the `DEBUG` environment variable by using a container label [1] such
    as `com.google.nginx-proxy.nginx-proxy.debug`.  I think doing so has
    dubious value, especially if we want to attempt backwards
    compatibility with the `DEBUG` environment variable.
    
    Fixes #2139
    
    [1] https://docs.docker.com/engine/reference/commandline/run/#-set-metadata-on-container--l---label---label-file
    
    Co-authored-by: Nicolas Duchon <nicolas.duchon@gmail.com>

--- test/test_debug/test_server-debug-flag.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import re
-
-def test_debug_info_is_present_in_nginx_generated_conf(docker_compose, nginxproxy):
-    conf = nginxproxy.get_conf().decode('ASCII')
-    assert re.search(r"# Exposed ports: \[\{[^}]+\s+80\s+tcp \} \{[^}]+\s+81\s+tcp \}\]", conf) or \
-           re.search(r"# Exposed ports: \[\{[^}]+\s+81\s+tcp \} \{[^}]+\s+80\s+tcp \}\]", conf)
-    assert conf.count("# Exposed ports: [{") == 1
