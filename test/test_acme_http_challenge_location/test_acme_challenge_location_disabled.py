c9b3807bacb937453668742879ee117c9dd01d59
Niek <100143256+SchoNie@users.noreply.github.com>
2024-05-31 15:46:58 +0200

    chore: spelling fixes for #2468

--- test/test_acme_http_challenge_location/test_acme_challenge_location_disabled.py
+++ test/test_acme_http_challenge_location/test_acme_challenge_location_disabled.py
@@ -15,14 +15,14 @@ def test_redirect_acme_challenge_location_enabled(docker_compose, nginxproxy, ac
     )
     assert r.status_code == 200
 
-def test_noderirect_acme_challenge_location_disabled(docker_compose, nginxproxy, acme_challenge_path):
+def test_noredirect_acme_challenge_location_disabled(docker_compose, nginxproxy, acme_challenge_path):
     r = nginxproxy.get(
         f"http://web3.nginx-proxy.tld/{acme_challenge_path}",
         allow_redirects=False
     )
     assert r.status_code == 404
 
-def test_noderirect_acme_challenge_location_enabled(docker_compose, nginxproxy, acme_challenge_path):
+def test_noredirect_acme_challenge_location_enabled(docker_compose, nginxproxy, acme_challenge_path):
     r = nginxproxy.get(
         f"http://web4.nginx-proxy.tld/{acme_challenge_path}",
         allow_redirects=False
--- test/test_acme_http_challenge_location/test_acme_challenge_location_legacy_is_default.py
+++ test/test_acme_http_challenge_location/test_acme_challenge_location_legacy_is_default.py
@@ -8,7 +8,7 @@ def test_redirect_acme_challenge_location_legacy(docker_compose, nginxproxy, acm
     )
     assert r.status_code == 200
 
-def test_noderirect_acme_challenge_location_legacy(docker_compose, nginxproxy, acme_challenge_path):
+def test_noredirect_acme_challenge_location_legacy(docker_compose, nginxproxy, acme_challenge_path):
     r = nginxproxy.get(
         f"http://web2.nginx-proxy.tld/{acme_challenge_path}",
         allow_redirects=False
