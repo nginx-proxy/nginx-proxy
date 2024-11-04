fdb7310cda59736cdaee9ea517dd963abf279e33
Nicolas Duchon <nicolas.duchon@gmail.com>
2024-11-04 23:03:01 +0100

    fix: do not render regexp hostname in debug endpoint response

--- test/test_debug_endpoint/test_global.py
+++ test/test_debug_endpoint/test_global.py
@@ -19,7 +19,7 @@ def test_debug_endpoint_response_contains_expected_values(docker_compose, nginxp
     assert jsonResponse["vhost"]["enable_debug_endpoint"] == True
 
 
-def test_debug_endpoint_pahts_stripped_if_response_too_long(docker_compose, nginxproxy):   
+def test_debug_endpoint_paths_stripped_if_response_too_long(docker_compose, nginxproxy):   
     r = nginxproxy.get("http://stripped.debug.nginx-proxy.example/nginx-proxy-debug")
     assert r.status_code == 200
     try:
@@ -29,6 +29,16 @@ def test_debug_endpoint_pahts_stripped_if_response_too_long(docker_compose, ngin
     if "paths" in jsonResponse["vhost"]:
         pytest.fail("Expected paths to be stripped from debug endpoint response", pytrace=False)
     assert jsonResponse["warning"] == "Virtual paths configuration for this hostname is too large and has been stripped from response."
+
+
+def test_debug_endpoint_hostname_replaced_by_warning_if_regexp(docker_compose, nginxproxy):   
+    r = nginxproxy.get("http://regexp.foo.debug.nginx-proxy.example/nginx-proxy-debug")
+    assert r.status_code == 200
+    try:
+        jsonResponse = json.loads(r.text)
+    except ValueError as err:
+        pytest.fail("Failed to parse debug endpoint response as JSON: %s" % err, pytrace=False)
+    assert jsonResponse["vhost"]["hostname"] == "Hostname is a regexp and unsafe to include in the debug response."
 
 
 def test_debug_endpoint_is_disabled_per_container(docker_compose, nginxproxy):
