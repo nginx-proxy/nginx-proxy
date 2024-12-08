84f8baa88e840dd75610857e2cf6b78455868879
Nicolas Duchon <nicolas.duchon@gmail.com>
2024-12-08 18:38:05 +0100

    tests: fix tests & test compose file

--- test/test_ssl/test_redirect.py
+++ test/test_ssl/test_redirect.py
@@ -4,31 +4,31 @@ import pytest
 # Permanent Redirects
 # https://github.com/nginx-proxy/nginx-proxy/pull/1737
 def test_web1_GET_301(docker_compose, nginxproxy):
-    r = nginxproxy.get('http://web1.nginx-proxy.tld', allow_redirects=False)
+    r = nginxproxy.get('http://nginx-proxy.tld', allow_redirects=False)
     assert r.status_code == 301
-    assert r.headers['Location'] == 'https://web1.nginx-proxy.tld/'
+    assert r.headers['Location'] == 'https://nginx-proxy.tld/'
 
 def test_web1_POST_308(docker_compose, nginxproxy):
-    r = nginxproxy.post('http://web1.nginx-proxy.tld', allow_redirects=False)
+    r = nginxproxy.post('http://nginx-proxy.tld', allow_redirects=False)
     assert r.status_code == 308
-    assert r.headers['Location'] == 'https://web1.nginx-proxy.tld/'
+    assert r.headers['Location'] == 'https://nginx-proxy.tld/'
 
 def test_web1_PUT_308(docker_compose, nginxproxy):
-    r = nginxproxy.put('http://web1.nginx-proxy.tld', allow_redirects=False)
+    r = nginxproxy.put('http://nginx-proxy.tld', allow_redirects=False)
     assert r.status_code == 308
-    assert r.headers['Location'] == 'https://web1.nginx-proxy.tld/'
+    assert r.headers['Location'] == 'https://nginx-proxy.tld/'
 
 def test_web1_HEAD_308(docker_compose, nginxproxy):
-    r = nginxproxy.head('http://web1.nginx-proxy.tld', allow_redirects=False)
+    r = nginxproxy.head('http://nginx-proxy.tld', allow_redirects=False)
     assert r.status_code == 308
-    assert r.headers['Location'] == 'https://web1.nginx-proxy.tld/'
+    assert r.headers['Location'] == 'https://nginx-proxy.tld/'
 
 def test_web1_DELETE_308(docker_compose, nginxproxy):
-    r = nginxproxy.delete('http://web1.nginx-proxy.tld', allow_redirects=False)
+    r = nginxproxy.delete('http://nginx-proxy.tld', allow_redirects=False)
     assert r.status_code == 308
-    assert r.headers['Location'] == 'https://web1.nginx-proxy.tld/'
+    assert r.headers['Location'] == 'https://nginx-proxy.tld/'
 
 def test_web1_OPTIONS_308(docker_compose, nginxproxy):
-    r = nginxproxy.options('http://web1.nginx-proxy.tld', allow_redirects=False)
+    r = nginxproxy.options('http://nginx-proxy.tld', allow_redirects=False)
     assert r.status_code == 308
-    assert r.headers['Location'] == 'https://web1.nginx-proxy.tld/'
+    assert r.headers['Location'] == 'https://nginx-proxy.tld/'
