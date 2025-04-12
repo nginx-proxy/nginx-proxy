dc08b2421418563ba592cdd88ba2f30c5b166f7f
lloydzhou <lloydzhou@qq.com>
2025-04-12 16:28:08 +0800

    revert to generate conf.d/10-upstream.conf

--- test/test_multiports/test_multiports-invalid-syntax.py
+++ test/test_multiports/test_multiports-invalid-syntax.py
@@ -11,7 +11,7 @@ def test_virtual_hosts_with_syntax_error_should_not_be_reachable(docker_compose,
 def test_config_should_have_multiports_warning_comments(docker_compose, nginxproxy):
     conf = nginxproxy.get_conf().decode('ASCII')
     matches = re.findall(r"the VIRTUAL_HOST_MULTIPORTS environment variable used for this container is not a valid YAML string", conf)
-    assert len(matches) % 3 == 0
+    assert len(matches) == 3
     assert "# invalidsyntax" in conf
     assert "# hostnamerepeat" in conf
     assert "# pathrepeat" in conf
