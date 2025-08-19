af1c403d74ed71c09d840f68eb68fa17e8aa6f18
Nicolas Duchon <nicolas.duchon@gmail.com>
2025-08-19 22:11:24 +0200

    test: use pytest-ignore-flaky instead of xfail

--- test/test_dockergen/test_dockergen_network_segregation.py
+++ test/test_dockergen/test_dockergen_network_segregation.py
@@ -1,13 +1,13 @@
 import pytest
 
 
-@pytest.mark.xfail(reason="flaky test")
+@pytest.mark.flaky
 def test_unknown_virtual_host_is_503(docker_compose, nginxproxy):
     r = nginxproxy.get("http://unknown.nginx-proxy.tld/")
     assert r.status_code == 503
 
 
-@pytest.mark.xfail(reason="flaky test")
+@pytest.mark.flaky
 def test_forwards_to_whoami(docker_compose, nginxproxy):
     r = nginxproxy.get("http://whoami2.nginx-proxy.tld/")
     assert r.status_code == 200
