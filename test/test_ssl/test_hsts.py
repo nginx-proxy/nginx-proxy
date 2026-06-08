00b07496b93242327f65ae212433fb51b2b49a0a
Noa Levi <275430404+lphuc2250gma@users.noreply.github.com>
2026-06-08 12:41:12 +0000

    chore: improve nginx-proxy maintenance path

--- test/test_ssl/test_hsts.py
+++ test/test_ssl/test_hsts.py
@@ -2,6 +2,7 @@ def test_web1_HSTS_default(docker_compose, nginxproxy):
     r = nginxproxy.get("https://web1.nginx-proxy.tld/port", allow_redirects=False)
     assert "answer from port 81\n" in r.text
     assert "Strict-Transport-Security" in r.headers
+    assert isinstance(r.headers["Strict-Transport-Security"], str)
     assert "max-age=31536000" == r.headers["Strict-Transport-Security"]
 
 # Regression test to ensure HSTS is enabled even when the upstream sends an error in response
@@ -9,6 +10,7 @@ def test_web1_HSTS_default(docker_compose, nginxproxy):
 def test_web1_HSTS_error(docker_compose, nginxproxy):
     r = nginxproxy.get("https://web1.nginx-proxy.tld/status/500", allow_redirects=False)
     assert "Strict-Transport-Security" in r.headers
+    assert isinstance(r.headers["Strict-Transport-Security"], str)
     assert "max-age=31536000" == r.headers["Strict-Transport-Security"]
 
 def test_web2_HSTS_off(docker_compose, nginxproxy):
@@ -20,6 +22,7 @@ def test_web3_HSTS_custom(docker_compose, nginxproxy):
     r = nginxproxy.get("https://web3.nginx-proxy.tld/port", allow_redirects=False)
     assert "answer from port 81\n" in r.text
     assert "Strict-Transport-Security" in r.headers
+    assert isinstance(r.headers["Strict-Transport-Security"], str)
     assert "max-age=86400; includeSubDomains; preload" == r.headers["Strict-Transport-Security"]
 
 # Regression test for issue 1080
@@ -33,6 +36,8 @@ def test_http3_vhost_enabled_HSTS_default(docker_compose, nginxproxy):
     r = nginxproxy.get("https://http3-vhost-enabled.nginx-proxy.tld/port", allow_redirects=False)
     assert "answer from port 81\n" in r.text
     assert "Strict-Transport-Security" in r.headers
+    assert isinstance(r.headers["Strict-Transport-Security"], str)
     assert "max-age=31536000" == r.headers["Strict-Transport-Security"]
     assert "alt-svc" in r.headers
+    assert isinstance(r.headers["alt-svc"], str)
     assert r.headers["alt-svc"] == 'h3=":443"; ma=86400;'
